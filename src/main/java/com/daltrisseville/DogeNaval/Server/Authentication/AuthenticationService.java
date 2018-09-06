e38f68d1f3eb671557d8d3b0d6a4ea0c9761bab2
EscalusFR <escalusfr@gmail.com>
2018-09-06 19:42:10 +0200

    Add files via upload

--- src/main/java/com/daltrisseville/DogeNaval/Server/Authentication/AuthenticationService.java
+++ src/main/java/com/daltrisseville/DogeNaval/Server/Authentication/AuthenticationService.java
@@ -1,13 +1,14 @@
 package com.daltrisseville.DogeNaval.Server.Authentication;
 
 import com.daltrisseville.DogeNaval.Server.Entities.ClientLoginEvent;
+import com.daltrisseville.DogeNaval.Users.UserHandler;
 
 public class AuthenticationService {
 
     public boolean authenticatePlayer(ClientLoginEvent clientLoginEvent) {
         if (clientLoginEvent.getEventCode().equals("LOGIN")) {
-            // todo
-            return true;
+            UserHandler handler = new UserHandler();
+            return handler.checkUserAuthentication(clientLoginEvent.getUsername(), clientLoginEvent.getPassword());
         } else {
             return false;
         }
